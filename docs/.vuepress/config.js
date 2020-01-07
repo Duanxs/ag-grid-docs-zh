6916ca7c6e29f0dcb2d45db7614db352a851c3e6
Duanxs <dxs@duanxs.com>
2020-01-07 23:49:12 +0800

    push

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,25 +1,24 @@
 module.exports = {
   title: 'ag-Grid 中文文档',
   description: 'ag-Grid 中文文档',
-  // base: '/Vue/',
+  base: '/ag-grid-docs-zh/',
   markdown: {
     lineNumbers: true
   },
   themeConfig: {
     displayAllHeaders: true,
     collapsable: false,
-    sidebar: {
-      '/': [{
+    sidebar: [
+      {
         title: 'Vue',
-        redirect: '/Vue/',
         children: [
-          '/Vue/',
-          '/Vue/Components',
-          '/Vue/VueMarkUp',
+          '',
+          'Vue',
+          'Components',
+          'VueMarkUp'
         ]
       }
-      ]
-    }
+    ]
   },
   plugins: [
     ['@vuepress/back-to-top', true],
